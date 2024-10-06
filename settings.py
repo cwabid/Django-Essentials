1c892eda47fc5beae48855450d542e4e0283968e
Code With Abid <86351779+cwabid@users.noreply.github.com>
2024-10-06 09:02:57 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 MEDIA_ROOT = BASE_DIR / 'media'
